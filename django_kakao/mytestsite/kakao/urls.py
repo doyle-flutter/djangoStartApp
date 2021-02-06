e04caf6127dc41067b1380d6637b9145ce9b77ab
doyle <il4479@hanmail.net>
2021-02-06 13:19:05 +0900

    djnago kakao

---
+++ django_kakao/mytestsite/kakao/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='kakao'),
+    path('kakaoLoginLogic/', views.kakaoLoginLogic),
+    path('kakaoLoginLogicRedirect/', views.kakaoLoginLogicRedirect),
+    path('kakaoLogout/', views.kakaoLogout),
+]
---
+++ django_kakao/mytestsite/kakao/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+import requests
+import json
+from django.template import loader
+# Create your views here.
+
+def index(request):
+    _context = {'check':False}
+    if request.session.get('access_token'):
+        _context['check'] = True
+    return render(request, 'index.html', _context)
+
+def kakaoLoginLogic(request):
+    _restApiKey = '' # 입력필요
+    _redirectUrl = 'http://127.0.0.1/kakaoLoginLogicRedirect'
+    _url = f'https://kauth.kakao.com/oauth/authorize?client_id={_restApiKey}&redirect_uri={_redirectUrl}&response_type=code'
+    return redirect(_url)
+
+def kakaoLoginLogicRedirect(request):
+    _qs = request.GET['code']
+    _restApiKey = '' # 입력필요
+    _redirect_uri = 'http://127.0.0.1/kakaoLoginLogicRedirect'
+    _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
+    _res = requests.post(_url)
+    _result = _res.json()
+    request.session['access_token'] = _result['access_token']
+    request.session.modified = True
+    return render(request, 'loginSuccess.html')
+
+def kakaoLogout(request):
+    _token = request.session['access_token']
+    _url = 'https://kapi.kakao.com/v1/user/logout'
+    _header = {
+      'Authorization': f'bearer {_token}'
+    }
+    _res = requests.post(_url, headers=_header)
+    _result = _res.json()
+    if _result.get('id'):
+        del request.session['access_token']
+        return render(request, 'loginoutSuccess.html')
+    else:
+        return render(request, 'logoutError.html')
